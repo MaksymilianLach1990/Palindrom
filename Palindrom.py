2466ef28b031618cca093d072b53c29acfdfc3fb
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-05-28 19:49:44 +0200

    Add loop

--- Palindrom.py
+++ Palindrom.py
@@ -1,26 +1,32 @@
 # Definicja sprawdzająca czy słowo jest palindromem
+while True:
+    # Pobierz słowo od urzytkownika
+    word = input()
+    letters = []
+    letters_revers = []
+    answer = None
+    # Przeliteruj słowo
+    for letter in word.lower():
+        letters.append(letter)
+        letters_revers.append(letter)
 
-# Pobierz słowo od urzytkownika
-word = input()
-print(word)
-letters = []
-letters_revers = []
-answer = None
-# Przeliteruj słowo
-for letter in word.lower():
-    letters.append(letter)
-    letters_revers.append(letter)
-print(letters)
-# Sprawdz liczbę liter
-number_of_letters = len(letters)
-print(number_of_letters)
-# Stwórz liste i ją odwruć
-letters_revers.reverse()
-print(letters_revers)
-# Sprawdz czy listy są sobie równe
-if letters == letters_revers:
-    answer = "jest"
-else:
-    answer = "nie jest"
-# Wyświetl informacje True \ False
-print("Twoje słowo: {}, {} palindromem.\nCzy chcesz spróbować jeszcze raz?(Y/N)".format(word, answer))
+    # Sprawd z liczbę liter
+    number_of_letters = len(letters)
+
+    # Stwórz liste i ją odwruć
+    letters_revers.reverse()
+
+    # Sprawdz czy listy są sobie równe
+    if letters == letters_revers:
+        answer = "jest"
+    else:
+        answer = "nie jest"
+    # Wyświetl informacje True \ False
+    if number_of_letters % 2 != 0:
+        answer = "nie jest"
+    print("Twoje słowo: {}, {} palindromem.\nCzy chcesz spróbować jeszcze raz?(Y/N)".format(word, answer))
+    decision = input()
+    if decision == 'Y':
+        continue
+    elif decision == 'N':
+        break
