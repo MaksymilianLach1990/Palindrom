f49c4b570d2048ed375da6d8b33e372deca9c323
Krzysztof Lach <lachkrzysztof56@gmail.com>
2021-05-29 07:34:17 +0200

    Create def and Add interface

--- Palindrom.py
+++ Palindrom.py
@@ -1,32 +1,39 @@
 # Definicja sprawdzająca czy słowo jest palindromem
-while True:
-    # Pobierz słowo od urzytkownika
-    word = input()
+def palindrom(name):
+    # Potrzebne zmienne
+    word = name
     letters = []
     letters_revers = []
-    answer = None
     # Przeliteruj słowo
     for letter in word.lower():
         letters.append(letter)
         letters_revers.append(letter)
-
-    # Sprawd z liczbę liter
-    number_of_letters = len(letters)
-
-    # Stwórz liste i ją odwruć
+    # Odwruć słowo
     letters_revers.reverse()
-
-    # Sprawdz czy listy są sobie równe
+    # Sprawdz rezultat
     if letters == letters_revers:
-        answer = "jest"
+        score = True
     else:
-        answer = "nie jest"
+        score = False
+    # Przekaż odpowiedz
+    return score
+
+
+# Rozpocznij zabawę
+while True:
+    # Przywitaj się
+    print("Proszę o podanie słowa do sprawdzenia:")
+    # Pobierz słowo od urzytkownika
+    check_the_word = input()
     # Wyświetl informacje True \ False
-    if number_of_letters % 2 != 0:
-        answer = "nie jest"
-    print("Twoje słowo: {}, {} palindromem.\nCzy chcesz spróbować jeszcze raz?(Y/N)".format(word, answer))
+    if palindrom(check_the_word) is True:
+        answer = 'jest'
+    else:
+        answer = 'nie jest'
+    print("Twoje słowo: {}, {} palindromem.\nCzy chcesz spróbować jeszcze raz?(Y/N)".format(check_the_word, answer))
     decision = input()
     if decision == 'Y':
         continue
     elif decision == 'N':
+        print("Exit")
         break
